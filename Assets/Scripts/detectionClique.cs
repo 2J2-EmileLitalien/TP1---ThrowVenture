70635b49a0de0cc655a0b4ebf4d20879a314a607
Emile Litalien <e2269456@cmaisonneuve.qc.ca>
2024-04-24 12:28:42 -0400

    Nouvelles balles
    Objets + Array pour cycler a travers les balles. Balle en or, magique et explosive ajouter (Explosive pas fini)
    
    Resolution en 16:9 (Donc deplacement des elements visuels qui sortait de l'ecran)
    
    +5 balles disponibles pour permettre de bien voir toutes les balles que le joueur peut utiliser

--- Assets/Scripts/detectionClique.cs
+++ Assets/Scripts/detectionClique.cs
@@ -5,6 +5,9 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System;
+using JetBrains.Annotations;
+using UnityEditor.Experimental.GraphView;
 
 public class detectionClique : MonoBehaviour
 {
@@ -17,11 +20,12 @@ public class detectionClique : MonoBehaviour
     private Vector3 positionSouris;
     private Vector3 directionDuLancer;
     private Vector2 positionInitiale;
+    private Vector2 positionBalleMagique;
 
     // Variables du score
     private float score = 0;
     public TextMeshProUGUI texteScore;
-    private float meilleurScore = 0;
+    public static float meilleurScore = 0;
     public TextMeshProUGUI texteMeilleurScore;
 
     // Variables de fin de partie
@@ -31,25 +35,105 @@ public class detectionClique : MonoBehaviour
     private float nmbDePegsToucher;
     public float nmbDePegsTotal;
 
+    // Variables changement de balles 
+    public Sprite balleMagiqueSprite;
+    public Sprite balleExplosiveSprite;
+    public Sprite balleOrSprite;
+    public Sprite balleNormaleSprite;
+
+    public class Balles
+    {
+        // General
+        public String nom;
+        public Sprite sprite;
+        public Boolean etat;
+
+        // Plus specifique
+        public float pointsMultiplicateur;
+        public float taille;
+        public int magique;
+        public Boolean explosion;
+    }
+
+    private Balles balleNormale;
+    private Balles balleMagique;
+    private Balles balleExplosive;
+    private Balles balleOr;
+
+    private Balles[] lesBalles;
+
+    private int pointeur = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        // Creation de mes balles + leurs informations
+            // BALLE NORMALE (Aucun changement)
+            balleNormale = new Balles();
+            balleNormale.nom = "Balle Normale";
+            balleNormale.sprite = balleNormaleSprite;
+            balleNormale.etat = true;
+            balleNormale.pointsMultiplicateur = 1f;
+            balleNormale.taille = 0.812f;
+            balleNormale.magique = 0;
+            balleNormale.explosion = false;
+
+            // BALLE MAGIQUE (Passe a travers de 1 peg)
+            balleMagique = new Balles();
+            balleMagique.nom = "Balle Magique"; 
+            balleMagique.sprite = balleMagiqueSprite; 
+            balleMagique.etat = true; 
+            balleMagique.pointsMultiplicateur = 1f; 
+            balleMagique.taille = 0.812f; 
+            balleMagique.magique = 1; 
+            balleMagique.explosion = false;
+
+            // BALLE EXPLOSIVE (Explose au contact)
+            balleExplosive = new Balles();
+            balleExplosive.nom = "Balle Explosive";
+            balleExplosive.sprite = balleExplosiveSprite;
+            balleExplosive.etat = true;
+            balleExplosive.pointsMultiplicateur = 1f;
+            balleExplosive.taille = 0.812f;
+            balleExplosive.magique = 0;
+            balleExplosive.explosion = true;
+
+            // BALLE EN OR (2x points)
+            balleOr = new Balles();
+            balleOr.nom = "Balle En Or";
+            balleOr.sprite = balleOrSprite;
+            balleOr.etat = true;
+            balleOr.pointsMultiplicateur = 2f;
+            balleOr.taille = 0.812f;
+            balleOr.magique = 0;
+            balleOr.explosion = false;
+        //
+
+        // On met les balles dans un array pour pouvoir les prendre en cycle (Chaque lancer = Une nouvelle) 
+            lesBalles = new Balles[] { balleNormale, balleMagique, balleExplosive, balleOr };
+        //
+
+        print("Balle actuelle = " + lesBalles[pointeur].nom);
+
+
         // Au lieu de set le corps rigide dans lancerLaBalle, donc le set a chaque clique, on le fait 1 fois
-        corpsRigideBalle = GetComponent<Rigidbody2D>();
+            corpsRigideBalle = GetComponent<Rigidbody2D>();
+        //
 
         // On sauvegarde la position initiale de la balle
-        positionInitiale = transform.position;
+            positionInitiale = transform.position;
+        //
 
 
         // On set le Meilleur Score qui sauvegarde a travers les parties
-        meilleurScore = PlayerPrefs.GetFloat("MeilleurScore");
-        texteMeilleurScore.text = "Meilleur score " + meilleurScore;
+            texteMeilleurScore.text = "Meilleur score " + meilleurScore;
+        //
 
         // On set la quantite de balles que le joueur peut lancer;
-        nmbDeBallesRestante = nmbDeBallesTotale;
-        texteBallesRestante.text = nmbDeBallesRestante + " / " + nmbDeBallesTotale;
+            nmbDeBallesRestante = nmbDeBallesTotale;
+            texteBallesRestante.text = nmbDeBallesRestante + " / " + nmbDeBallesTotale;
+        //
     }
 
     // Update is called once per frame
@@ -107,24 +191,89 @@ public class detectionClique : MonoBehaviour
     {
         if (collision.gameObject.tag == "fondDuPlateauDeJeu")
         {
-            // Replace la balle, on enleve la simulation, la rend disponible
-            transform.position = positionInitiale;
-            corpsRigideBalle.simulated = false;
-            balleDisponible = true;
+            // Si balle actuelle peut revenir en haut (Magique), sinon remplacer la balle 
+            if (lesBalles[pointeur].magique > 0)
+            {
+            // La balle est magique + peut revenir en haut
 
-        } else if (collision.gameObject.tag == "pegBleu")
+                // On fait -1 a la quantiter de fois ou elle peut revenir en haut...
+                    lesBalles[pointeur].magique -= 1;
+                //
+
+                // ... et on la remplace en haut
+                positionBalleMagique = new Vector2(transform.position.x, positionInitiale.y);
+                transform.position = positionBalleMagique;
+                //
+
+            } else
+            {
+            // Remplacer la balle, etc.
+
+                // Replace la balle, on enleve la simulation, la rend disponible
+                    transform.position = positionInitiale;
+                    corpsRigideBalle.simulated = false;
+                    balleDisponible = true;
+                //
+
+                // On reset le magique si la balle est la magique AVANT de changer de balle
+                    if (lesBalles[pointeur].nom == "Balle Magique")
+                    {
+                        lesBalles[pointeur].magique = 1;
+                    }
+                //
+
+                // On cycle a travers les balles apres chaque lancer
+                    if (pointeur < lesBalles.Length-1)
+                    {
+                        pointeur += 1;
+                    } else
+                    {
+                        pointeur = 0;
+                    }
+                //
+                
+                // On change le sprite de la balle 
+                    GetComponent<SpriteRenderer>().sprite = lesBalles[pointeur].sprite;
+                //
+                print("Balle actuelle = " + lesBalles[pointeur].nom);
+            }
+
+    } else if (collision.gameObject.tag == "limiteDuJeu")
         {
-            // On enleve le peg et ajoute au score
-            nmbDePegsToucher += 1;
-            collision.gameObject.SetActive(false);
-            score += 1;
-   
-        } else if (collision.gameObject.tag == "pegJaune")
+            // Ne rien faire si limite de jeu 
+        }
+        else
         {
-            // On enleve le peg et ajoute au score
-            nmbDePegsToucher += 1;
-            collision.gameObject.SetActive(false);
-            score += 3;
+         // 100% collision = avec un peg
+
+
+            // EFFET DE BALLE SELON LE POINTEUR 
+
+            if (lesBalles[pointeur].explosion)
+            {
+            // SI BALLE = PEUT EXPLOSER
+
+            }
+
+            // On enleve le peg et on ajoute qu'un peg a ete toucher...
+                nmbDePegsToucher += 1;
+                collision.gameObject.SetActive(false);
+            //
+
+            // ...et on arrange le score selon le peg toucher
+                if (collision.gameObject.tag == "pegBleu")
+                {
+                    score += lesBalles[pointeur].pointsMultiplicateur * 1;
+
+                }
+                else if (collision.gameObject.tag == "pegJaune")
+                {
+                    score += lesBalles[pointeur].pointsMultiplicateur * 3;
+                }
+            //
+
+            
+
         }
 
         // Peut importe la collision, on re-affiche le score (Englobe les collisions pegs, collision avec zone de fin qui reset la balle, etc.)
@@ -134,12 +283,7 @@ public class detectionClique : MonoBehaviour
 
     void finDePartie()
     {
-        // On sauvegarde le meilleur score 
-        PlayerPrefs.SetFloat("MeilleurScore", meilleurScore);
-
-
         // print("Meilleur score on end = " + meilleurScore);
-        // print("MeilleurScore de PlayerPrefs en fin de partie = " + PlayerPrefs.GetFloat("MeilleurScore"));
 
         // On reset la scene
         SceneManager.LoadScene("SampleScene");
